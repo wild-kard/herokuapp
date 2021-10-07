d1665ba10651b776999f7f29d7521488eb124fe4
Collin-Falstad <wildkard91@gmail.com>
2021-10-07 19:40:42 -0400

    built server

--- index.js
+++ index.js
@@ -5,12 +5,19 @@ const cors = require("cors")
 
 const port = process.env.PORT || 9000
 
+const path = require('path')
+
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname,"client/build")))
 
 //underscore is a place holder variable
 app.use("/api/*", (_,res)=>{
     res.json({data:"This is the API data"})
+})
+
+app.use("*", (_,res)=>{
+    res.sendFile(path.join(__dirname,"client/build","index.html"))
 })
 
 app.listen(port,()=>{
